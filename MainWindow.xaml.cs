c04b3c33862879038a9e4ba135cd0b3970ca4187
joshtharakan <josh_tharakan@hotmail.com>
2016-07-18 14:48:49 +0530

    initial commit
    test

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="saramgsilva">
+//   Copyright (c) 2014 saramgsilva. All rights reserved.
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Windows.Media;
+using FirstFloor.ModernUI.Presentation;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace ModernUIForWPFSample.Navigation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml.
+    /// </summary>
+    public partial class MainWindow 
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public ICommand ResourcesCommand { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppearanceManager.Current.AccentColor = Colors.Orange;
+            ContentSource = MenuLinkGroups.First().Links.First().Source;
+            ResourcesCommand = new GalaSoft.MvvmLight.Command.RelayCommand(ShowResources);
+        }
+        /// <summary>
+        /// Shows the resources.
+        /// </summary>
+        private void ShowResources()
+        {
+        }
+    }
+}
---
+++ Services/IModernNavigationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight.Views;
+namespace ModernUIForWPFSample.Navigation__MVVM_.Services
+{
+    /// <summary>
+    /// The ModernNavigationService interface.
+    /// </summary>
+    public interface IModernNavigationService : INavigationService
+    {
+        /// <summary>
+        /// Gets the parameter.
+        /// </summary>
+        /// <value>
+        /// The parameter.
+        /// </value>
+        object Parameter { get; }
+    }
+}
